5fb37f0c25e6ab2a7a1c3cecdaba4c64d5cd7f99
staufferschool3 <stauffer.school.3@gmail.com>
2026-02-09 15:34:26 -0500

    Add files via upload

---
+++ Alnir_Game/Alnir_assets/Equipment.py
@@ -0,0 +1,8 @@
+# Equipment Manager
+class EquipmentMan:
+    def weapon(self, Damage=None):
+        self.weapon.dmg = Damage
+
+# Rusty Sword
+def rusty_sword():
+    Rusty_sword = EquipmentMan.weapon(40)
