e4a958141e20ed78308a85cd3b7a28b39c0328b3
staufferschool3 <stauffer.school.3@gmail.com>
2026-02-09 16:12:19 -0500

    Update Equipment.py

--- Alnir_Game/Alnir_assets/Equipment.py
+++ Alnir_Game/Alnir_assets/Equipment.py
@@ -1,8 +1,23 @@
+# Game imports
+from Alnir_Game import Enemy
+
+# Instancing Enemy
+Enemy_critter = Enemy()
+
 # Equipment Manager
 class EquipmentMan:
-    def weapon(self, Damage=None):
+    def __init__(self):
+        None
+
+    def weapon(self, Damage=None, Effect=None):
         self.weapon.dmg = Damage
+        self.weapon.effect = Effect
+
+    def effect(self, Effect=None):
+        self.effect = Effect
 
 # Rusty Sword
-def rusty_sword():
+def rusty_sword(Enemy_critter):
+    Tetnis = Enemy_critter.agi(-10)
     Rusty_sword = EquipmentMan.weapon(40)
+    Rusty_sword = EquipmentMan.effect(Tetnis)
